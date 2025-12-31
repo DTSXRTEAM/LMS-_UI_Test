a6abbb040bb3eb71acc2e4d20397775e7a6f5d25
DTSXRTEAM <137754320+DTSXRTEAM@users.noreply.github.com>
2025-12-31 15:44:45 +0530

    forward and backward section using buttons updated

--- Assets/Scripts/CourseContent/CourseContentManager.cs
+++ Assets/Scripts/CourseContent/CourseContentManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Video;
 
 [System.Serializable]
 public class CourseChapter
@@ -10,10 +11,19 @@ public class CourseChapter
     public List<GameObject> CourseSectionCards;
 }
 
+public enum SectionContentType
+{
+    Video,
+    Text
+}
+
 [System.Serializable]
 public class CourseSection
 {
     public string CourseSectionTitle;
+    public SectionContentType contentType;
+    public GameObject textContent;
+    public GameObject videoContainer;  
 }
 
 public class CourseContentManager : MonoBehaviour
@@ -21,7 +31,12 @@ public class CourseContentManager : MonoBehaviour
     public Transform Container;
     public GameObject ChapterCardPrefab;
     public GameObject SectionCardPrefab;
-
+    int currentChapter = -1;
+    int currentSection = -1;
+    public VideoPlayer videoPlayer;
+    public GameObject videoPanel;
+    GameObject activeContent;
+    public GameObject textPanel;
     public List<CourseChapter> m_courseChapters = new List<CourseChapter>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -39,11 +54,25 @@ public class CourseContentManager : MonoBehaviour
         
             for(int j=0; j < m_courseChapters[i].CourseSection.Count; j++)
             {
+                int sectionIndex = j;
+
                 GameObject Sectioncard = Instantiate(SectionCardPrefab, Container);
                 Sectioncard.GetComponent<SectionCardReference>().SectionTitle.text = $"Section {j+1} : {m_courseChapters[i].CourseSection[j].CourseSectionTitle}";
                 m_courseChapters[i].CourseSectionCards.Add(Sectioncard);
+
+                Sectioncard.GetComponent<SectionCardReference>()
+                .ClickButton
+                .onClick
+                .AddListener(() => OnSectionClicked(chapterIndex, sectionIndex));
             }
-        }        
+        }
+
+            // Auto-select first section on start (optional but recommended)
+        if (m_courseChapters.Count > 0 &&
+            m_courseChapters[0].CourseSection.Count > 0)
+        {
+            OnSectionClicked(0, 0);
+        }  
     }
 
     public void toggleCard(int chatercard)
@@ -51,13 +80,86 @@ public class CourseContentManager : MonoBehaviour
         foreach (GameObject item in m_courseChapters[chatercard].CourseSectionCards)
         {
             if (item.activeSelf)
-            {
                 item.SetActive(false);
-            }
             else
-            {
                 item.SetActive(true);
-            }
         }
     }
+
+  void OnSectionClicked(int chapterIndex, int sectionIndex)
+{
+    currentChapter = chapterIndex;
+    currentSection = sectionIndex;
+
+    // Hide previous content
+    if (activeContent != null)
+    {
+        // Stop video if previous was a video container
+        VideoPlayer vp = activeContent.GetComponentInChildren<VideoPlayer>();
+        if (vp != null && vp.isPlaying)
+            vp.Stop();
+
+        activeContent.SetActive(false);
+    }
+
+    CourseSection section =
+        m_courseChapters[chapterIndex].CourseSection[sectionIndex];
+
+    if (section.contentType == SectionContentType.Text)
+    {
+        activeContent = section.textContent;
+    }
+    else // Video
+    {
+        activeContent = section.videoContainer;
+    }
+
+    if (activeContent != null)
+        activeContent.SetActive(true);
+}
+
+public void NextSection()
+{
+    if (currentChapter == -1 || currentSection == -1)
+        return;
+
+    int nextSection = currentSection + 1;
+
+    // Stop if last section
+    if (nextSection >= m_courseChapters[currentChapter].CourseSection.Count)
+        return;
+
+    OnSectionClicked(currentChapter, nextSection);
+}
+
+public void PreviousSection()
+{
+    if (currentChapter == -1 || currentSection == -1)
+        return;
+
+    int prevSection = currentSection - 1;
+
+    // Stop if first section
+    if (prevSection < 0)
+        return;
+
+    OnSectionClicked(currentChapter, prevSection);
+}
+
+    /*void ShowVideoPage(CourseSection section)
+    {
+        textPanel.SetActive(false);
+        videoPanel.SetActive(true);
+
+        // Hook VideoPlayer here (DO NOT autoplay)
+        Debug.Log("Open VIDEO section: " + section.CourseSectionTitle);
+    }
+
+    void ShowTextPage(CourseSection section)
+    {
+        videoPanel.SetActive(false);
+        textPanel.SetActive(true);
+
+        Debug.Log("Open TEXT section: " + section.CourseSectionTitle);
+    }*/
 }
--- Assets/Scripts/CourseContent/SectionCardReference.cs
+++ Assets/Scripts/CourseContent/SectionCardReference.cs
@@ -1,7 +1,9 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SectionCardReference : MonoBehaviour
 {
 public TextMeshProUGUI SectionTitle;
+public Button ClickButton;
 }
